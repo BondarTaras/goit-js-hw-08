c9c99b8edbf5a522f10cbbc9a084cd4b77bb823e
Taras Bondar <tarasbondar2@gmail.com>
2023-06-18 19:31:39 +0300

    dz

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onSaveCurrentTime = function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
+};
+
+player.on('timeupdate', throttle(onSaveCurrentTime, 1000));
+
+const lastTime = localStorage.getItem('videoplayer-current-time');
+if (lastTime) {
+  player.setCurrentTime(lastTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInput(evn) {
+  try {
+    formData[evn.target.name] = evn.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+
+populateForm();
+
+function onFormSubmit(evn) {
+  evn.preventDefault();
+  const {
+    elements: { email, message },
+  } = evn.currentTarget;
+
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  const object = { email: email.value, message: message.value };
+
+  console.log(object);
+  evn.currentTarget.reset();
+
+  formData = {};
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// отримуємо значення із сховища, розпарсимо
+// якщо там щось було, оновлюємо DOM
+// застосовуємо try...catch
+
+function populateForm() {
+  try {
+    const savedSettings = localStorage.getItem(STORAGE_KEY);
+    const parsedSettings = JSON.parse(savedSettings);
+
+    if (parsedSettings) {
+      formData = parsedSettings;
+      for (const field of form) {
+        if (formData[field.name]) {
+          field.value = formData[field.name];
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
